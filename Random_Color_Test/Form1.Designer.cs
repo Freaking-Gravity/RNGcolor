ddf6be5145175df619cf4c1976fbe734e4b32932
Matthew Jensen <zzzxxxyxzzy@gmail.com>
2020-03-12 17:55:06 -0600

    init

---
+++ Random_Color_Test/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Random_Color_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Color_Generate_Button = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Color_Generate_Button
+            // 
+            this.Color_Generate_Button.Location = new System.Drawing.Point(329, 396);
+            this.Color_Generate_Button.Name = "Color_Generate_Button";
+            this.Color_Generate_Button.Size = new System.Drawing.Size(95, 42);
+            this.Color_Generate_Button.TabIndex = 0;
+            this.Color_Generate_Button.Text = "Change Color";
+            this.Color_Generate_Button.UseVisualStyleBackColor = true;
+            this.Color_Generate_Button.Click += new System.EventHandler(this.Color_Generate_Button_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Red;
+            this.Exit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Exit.FlatAppearance.BorderSize = 5;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit.Location = new System.Drawing.Point(770, 1);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(28, 28);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Color_Generate_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Color_Generate_Button;
+        private System.Windows.Forms.Button Exit;
+    }
+}
+
---
+++ Random_Color_Test/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Color_Test
+{
+    public partial class Form1 : Form
+    {
+        private Random rngColor = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Color_Generate_Button_Click(object sender, EventArgs e)
+        {
+            Color randomColor = Color.FromArgb(rngColor.Next(256), rngColor.Next(256), rngColor.Next(256));
+            changeColor(randomColor);
+        }
+        void changeColor(Color color)
+        {
+            this.BackColor = color;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
